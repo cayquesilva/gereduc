00b4b1810cae9b105b21951a09e1c4bf3528d945
CayqueSilva <cayque.ti@gmail.com>
2021-02-28 13:34:59 -0300

    test

--- GerEduc/index.php
+++ GerEduc/index.php
@@ -11,9 +11,75 @@
     <title>GerEduc</title>
 </head>
 <body>
-    gerenciamento educacional
-    gereduc!!!
-    teste de branch
-    --teste de merge branch2
+    <header class="container">
+            <div class="top-bar">
+                <div class="Menu">
+                    menu
+                </div><!--menu-->
+                <div class="logo">Logo</div><!--logo-->
+                <div class="social"><!--social-->
+                    Face
+                    Insta
+                </div>
+            </div><!--top-bar-->
+    </header>
+
+    <section class="main container">
+        <div class="navegacao">
+            <nav class="nav-menu">
+                <ul>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </nav><!--nav-menu-->
+        </div><!--navegacao-->
+        <div class="main-conteudo">
+            <div class="box-conteudo">
+                <div class="box-individual">
+                    <h1>Título de algo</h1>
+                    <ul>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div><!--box-individual-->
+                <div class="box-individual">
+                    <h1>Título de algo</h1>
+                    <ul>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div><!--box-individual-->
+                <div class="box-individual">
+                    <h1>Título de algo</h1>
+                    <ul>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div><!--box-individual-->
+                <div class="box-individual">
+                    <h1>Título de algo</h1>
+                    <ul>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div><!--box-individual-->
+            </div><!--box-conteudo-->
+        </div><!--main-conteudo-->
+    </section><!--main-->
 </body>
 </html>
