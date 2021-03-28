26ef28ba0442802de66b04de34a752974fca6fc4
CayqueSilva <cayque.ti@gmail.com>
2021-03-28 12:44:33 -0300

    Edição de slides

--- painel/pages/editar-slide.php
+++ painel/pages/editar-slide.php
@@ -11,34 +11,51 @@
     <h2><i class="fas fa-pen"></i> Editar Slide</h2>
     <!--enctype serve para permitir envio de imagem no formulário-->
     <form method=post enctype="multipart/form-data">
+
     <?php 
 
         if(isset($_POST['acao'])){
             $nome = $_POST['nome'];
-            $imagem = $_FILES['slide'];
-            if($nome == '')
-                Painel::alerta('erro','O Nome está vazio!');
-            else if(Painel::imagemValida($imagem) == false){
-                Painel::alerta('erro','A imagem não é de um formato válido.');
-                }else{
+            $imagem = $_FILES['imagem'];
+            $imagem_atual = $_POST['imagem_atual'];
+            if($imagem['name'] != ''){
+                //imagem foi escolhida
+                if(Painel::imagemValida($imagem)){
+                    //deleta imagem existente para livrar o bd
+                    Painel::deleteFile($imagem_atual);
+                    //faz upload de imagem
                     $imagem = Painel::uploadFile($imagem);
-                    //posso usar assim ou colocar em inputs "hidden"
-                    $arr = ['nome'=>$nome,'slide'=>$imagem,'id_ordem'=>0,'nome_tabela'=>'tb_site.slide'];
-                    Painel::insert($arr);
-                    Painel::alerta('sucesso','O cadastro do slide foi realizado!');
+                    $arr = ['id'=>$id,'nome'=>$nome,'slide'=>$imagem,'nome_tabela'=>'tb_site.slide'];
+                    Painel::editarItem($arr);
+                    $slide = Painel::selectItem('tb_site.slide',$id);
+                    Painel::alerta('sucesso','O slide foi editado com sucesso!');
+                }else{
+                    Painel::alerta('erro','O formato da imagem não é válido');
                 }
-            };
+            }else{
+                //imagem não selecionada
+                $imagem = $imagem_atual;
+                $arr = ['id'=>$id,'nome'=>$nome,'slide'=>$imagem,'nome_tabela'=>'tb_site.slide'];
+                Painel::editarItem($arr);
+                $slide = Painel::selectItem('tb_site.slide',$id);
+                Painel::alerta('sucesso','O nome do slide foi alterado com sucesso!');
+            }
+        }
+
     ?>
+
+
     <div class=form-group>
-        <label>Nome do slide:</label>
-        <input type="text" name="nome" value="<?php echo $slide['nome']?>">
+        <label>Nome:</label>
+        <input type="text" name="nome" required value="<?php echo $slide['nome']; ?>">
     </div><!--form-group-->
     <div class=form-group>
         <label>Imagem:</label>
-        <input type="file" name="slide">
+        <input type="file" name="imagem">
+        <input type="hidden" name="imagem_atual" value="<?php echo $slide['slide']; ?>">
     </div><!--form-group-->
     <div class=form-group>
-        <input type="submit" name="acao" value="Cadastrar" >
+        <input type="submit" name="acao" value="Atualizar" >
     </div><!--form-group-->
     </form>
 </div><!--box-content-->
